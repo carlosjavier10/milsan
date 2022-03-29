3b2b0cd4196159c46c2aff6f33bcbfa7b0914a49
Carlos Javier <carlosjavier10@gmail.com>
2022-03-29 09:35:02 -0400

    cabecera bizpage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,51 +1,61 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name') }} @yield('title') </title>
-        <link rel="icon" href=" {{ asset('/images/MSarquitecta favicon.svg')}} " sizes="any" type="image/svg+xml">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+<head>
+  <meta charset="utf-8">
+   <title>{{ config('app.name') }} @yield('title') </title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-       <link rel="stylesheet" href="{{ asset('/css/laravel.css') }}">
-       <link rel="stylesheet" href="{{ asset('/css/nav-menu.css') }}">
+  <!-- Favicons -->
+  <link rel="icon" href=" {{ asset('/images/MSarquitecta favicon.svg')}} " sizes="any" type="image/svg+xml">
+{{--   <link href=" {{ asset('/images/apple-touch-icon.png') }} " rel="apple-touch-icon"> --}}
 
-        <!-- Styles -->
-        <style>
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
 
+  <!-- Bootstrap CSS File -->
+  <link href=" {{ asset('lib/bootstrap/css/bootstrap.min.css') }} " rel="stylesheet">
 
+  <!-- Libraries CSS Files -->
+  <link href=" {{ asset('lib/font-awesome/css/font-awesome.min.css') }}  " rel="stylesheet">
+  <link href=" {{ asset('lib/animate/animate.min.css') }} " rel="stylesheet">
+  <link href=" {{ asset('lib/ionicons/css/ionicons.min.css') }} " rel="stylesheet">
+  <link href=" {{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }} " rel="stylesheet">
+  <link href=" {{ asset('lib/lightbox/css/lightbox.min.css') }} " rel="stylesheet">
 
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-
-        <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <script src="https://kit.fontawesome.com/3851450290.js" crossorigin="anonymous"></script>
-
-    </head>
-
-    @include('layouts.nav-menu')
-
-     @yield('content')
-
-    <body class="antialiased">
+  <!-- Main Stylesheet File -->
+  <link href=" {{ asset('css/style.css') }}  " rel="stylesheet">
 
 
+</head>
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<body>
 
-        <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+@include('layouts.nav-menu')
 
-        <script>
-            $(function() { $(".toggle").on("click", function() { if ($(".item").hasClass("active")) { $(".item").removeClass("active"); $(this).find("a").html("<i class='fas fa-bars'></i>"); } else { $(".item").addClass("active"); $(this).find("a").html("<i class='fas fa-times'></i>"); } }); });
-        </script>
-    </body>
+@include('layouts.carrousel')
+
+
+
+  <!-- JavaScript Libraries -->
+  <script src=" {{ asset('lib/jquery/jquery.min.js') }} "></script>
+  <script src=" {{ asset('lib/jquery/jquery-migrate.min.js') }} "></script>
+  <script src=" {{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
+  <script src=" {{ asset('lib/easing/easing.min.js') }} "></script>
+  <script src=" {{ asset('lib/superfish/hoverIntent.js') }} "></script>
+  <script src=" {{ asset('lib/superfish/superfish.min.js') }} "></script>
+  <script src=" {{ asset('lib/wow/wow.min.js') }} "></script>
+  <script src=" {{ asset('lib/waypoints/waypoints.min.js') }} "></script>
+  <script src=" {{ asset('lib/counterup/counterup.min.js') }} "></script>
+  <script src=" {{ asset('lib/owlcarousel/owl.carousel.min.js') }} "></script>
+  <script src=" {{ asset('lib/isotope/isotope.pkgd.min.js') }} "></script>
+  <script src=" {{ asset('lib/lightbox/js/lightbox.min.js') }} "></script>
+  <script src=" {{ asset('lib/touchSwipe/jquery.touchSwipe.min.js') }} "></script>
+  <!-- Contact Form JavaScript File -->
+  <script src=" {{ asset('contactform/contactform.js') }} "></script>
+
+  <!-- Template Main Javascript File -->
+  <script src=" {{ asset('js/main.js') }} "></script>
+</body>
 </html>
--- resources/views/layouts/nav-menu.blade.php
+++ resources/views/layouts/nav-menu.blade.php
@@ -1,58 +1,32 @@
-  <nav>
-    <ul class="menu nav">
-      <li class="logo">
-        {{-- <a href="#"><img class="logoh" src="{{ asset('/storage/images/logo_header_white.png') }}"></a> --}}
-        <a href="#"><img class="logoh" src="{{ asset('/images/MSarquitecta logo header black.png') }}"></a>
-      </li>
-      <li class="item nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="item nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-  <li class="item nav-item">
-        <a class="nav-link" href="#">Services</a>
-      </li>
-  <li class="item nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-  <li class="item nav-item">
-        <a class="nav-link" href="#">Blog</a>
-      </li>
-  <li class="item nav-item">
-        <a class="nav-link {{ request()->routeIs('contactanos.index') ? 'active' : '' }}" href="{{ route('contactanos.index') }} ">Contact</a>
-{{--    </li>
-      <li class="itaem button">
-        <a href="#">Log In</a>
-      </li>
-      <li class="item button secondary">
-        <a href="#">Sign Up</a>
-      </li> --}}
+ <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
 
-      <li class="item">
-        <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-            {{__('messages.language')}}
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="locale/en">{{__('messages.english')}}</a></li>
-            <li><a class="dropdown-item" href="locale/es">{{__('messages.spanish')}} </a></li>
+      <div id="logo" class="pull-left ">
+       {{--  <h1><a href="#intro" class="scrollto">BizPage</a></h1> --}}
+        <!-- Uncomment below if you prefer to use an image logo -->
+         <a href="#intro"><img class="logoms" src=" {{ asset('/images/MSarquitecta logo Horizontal logo dorado.png') }}  " alt="" title="" /></a>
+      </div>
 
-          </ul>
-        </div>
-      </li>
-
-
-
-      <li class="toggle">
-        <a href="#">
-          <i class="fas fa-bars"> </i>
-        </a>
-      </li>
-
-    </ul>
-
-
-
-
-  </nav>
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li class="menu-has-children"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
